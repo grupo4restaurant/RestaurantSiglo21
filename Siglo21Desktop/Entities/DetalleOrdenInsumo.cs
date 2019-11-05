7f15160c84e22a51b2bbf68cc67dc6e09535ed8a
Jhon Carrasco Albornoz <jhon.carrasco@ignite.cl>
2019-11-05 17:46:08 -0300

    entidades 2
    nuevas

---
+++ Siglo21Desktop/Entities/DetalleOrdenInsumo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Siglo21Desktop.Entities
+{
+    class DetalleOrdenInsumo
+    {
+        public int det_ord_insumo_id { get; set; }
+        public int orden_insumo_id { get; set; }
+        public int producto_id { get; set; }
+        public int um_id { get; set; }
+        public int cantidad { get; set; }
+        public int estado { get; set; }
+    }
+}
---
+++ Siglo21Desktop/Entities/DetalleOrdenMesa.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Siglo21Desktop.Entities
+{
+    class DetalleOrdenMesa
+    {
+        public int det_ord_mesa_id { get; set; }
+        public int menu_id { get; set; }
+        public int order_mesa_id { get; set; }
+        public int cantidad { get; set; }
+        public int estado { get; set; }
+    }
+}
