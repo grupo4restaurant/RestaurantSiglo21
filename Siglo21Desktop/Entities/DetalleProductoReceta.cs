90d5943214c89db924f796b1be9ee440e7c066c6
Jhon Carrasco Albornoz <jhon.carrasco@ignite.cl>
2019-11-05 17:55:08 -0300

    entidades 3
    detalle_producto_receta

---
+++ Siglo21Desktop/Entities/DetalleProductoReceta.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Siglo21Desktop.Entities
+{
+    class DetalleProductoReceta
+    {
+        public int det_pro_receta_id { get; set; }
+        public int receta_id { get; set; }
+        public int producto_id { get; set; }
+        public int um_id { get; set; }
+        public int cantidad { get; set; }
+    }
+}
