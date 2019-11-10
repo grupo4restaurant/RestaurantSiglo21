5e82edbc89fd019662537391698c9cdbcf5146cb
Jhon Carrasco Albornoz <jhon.carrasco@ignite.cl>
2019-11-10 02:01:36 -0300

    actualizacion entity clase reserva

--- Siglo21Desktop/Entities/Reserva.cs
+++ Siglo21Desktop/Entities/Reserva.cs
@@ -9,11 +9,13 @@ namespace Siglo21Desktop.Entities
     class Reserva
     {
         public int reserva_id { get; set; }
-        public int mesa_id { get; set; }
         public DateTime fecha { get; set; }
         public int cantidad_persona { get; set; }
         public int rut_cliente { get; set; }
         public string e_mail_cliente { get; set; }
         public string fono_cliente { get; set; }
+        public string nombre { get; set; }
+        public string hora_inicio { get; set; }
+        public string comentarios { get; set; }
     }
 }
