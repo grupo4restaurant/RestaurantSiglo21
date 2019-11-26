5e93d7a7acc320542611d3720218ff3de924081f
Jhon Carrasco Albornoz <jhon.carrasco@ignite.cl>
2019-11-26 18:26:12 -0300

    example datagrid Recursos-Mesa

--- Siglo21Desktop/Control/Recursos/RecursosMenuUC.xaml.cs
+++ Siglo21Desktop/Control/Recursos/RecursosMenuUC.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Siglo21Desktop.Entities;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +25,60 @@ namespace Siglo21Desktop.Control.Recursos
         public RecursosMenuUC()
         {
             InitializeComponent();
+            //create business data
+            var itemList = new List<StockItem>();
+            itemList.Add(new StockItem { Name = "Many items", Quantity = 100, IsObsolete = false });
+            itemList.Add(new StockItem { Name = "Enough items", Quantity = 10, IsObsolete = false });
+            itemList.Add(new StockItem { Name = "Shortage item", Quantity = 1, IsObsolete = false });
+            itemList.Add(new StockItem { Name = "Item with error", Quantity = -1, IsObsolete = false });
+            itemList.Add(new StockItem { Name = "Obsolete item", Quantity = 200, IsObsolete = true });
+
+            //link business data to CollectionViewSource
+            CollectionViewSource itemCollectionViewSource;
+            itemCollectionViewSource = (CollectionViewSource)(FindResource("ItemCollectionViewSource"));
+            itemCollectionViewSource.Source = itemList;
         }
+
+        
+        
+        
+        private void btnView_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                StockItem dataRowView = (StockItem)((Button)e.Source).DataContext;
+                String ProductName = dataRowView.Name;
+                String ProductDescription = dataRowView.Quantity.ToString();
+                MessageBox.Show("You Clicked : " + ProductName + "\r\nDescription : " + ProductDescription);
+                //This is the code which will show the button click row data. Thank you.
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                StockItem dataRowView = (StockItem)((Button)e.Source).DataContext;
+                String ProductName = dataRowView.Name;
+                String ProductDescription = dataRowView.Quantity.ToString();
+                MessageBox.Show("You Clicked : " + ProductName + "\r\nDescription : " + ProductDescription + "\r\nBorrado!!!");
+                //This is the code which will show the button click row data. Thank you.
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+        }
+
+
+
+
+
+
     }
 }
+    
