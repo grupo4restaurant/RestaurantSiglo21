30700ed1d4d4cfb3784fa2b84d48d17deeccd273
Jhon Carrasco Albornoz <jhon.carrasco@ignite.cl>
2019-11-26 12:15:42 -0300

    danitza 26-11-2019
    cambio de nombre de las carpetas formularios

---
+++ Siglo21Desktop/Formulario/Recursos/ProveedorForm/IngresoDeProveedores.xaml.cs
@@ -0,0 +1,63 @@
+﻿using Siglo21Desktop.Dao;
+using Siglo21Desktop.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Siglo21Desktop.Formulario.Recursos.ProveedorForm
+{
+    /// <summary>
+    /// Interaction logic for IngresoDeProveedores.xaml
+    /// </summary>
+    public partial class IngresoDeProveedores : Window
+    {
+        public IngresoDeProveedores()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGuardar_Click(object sender, RoutedEventArgs e)
+        {
+            var textoNombre = txtNombre.Text;
+            var textoFono = txtFono.Text;
+            var textoContacto = txtContacto.Text;
+            var textoEmail = txtEmail.Text;
+            var textoDireccion = txtDireccion.Text;
+            var textoComuna = txtComuna.Text;
+
+            ProveedorDAO proveedorDao = new ProveedorDAO();
+       
+            try
+            {
+                Proveedor obj = new Proveedor()
+                {
+
+                    nombre = textoNombre,
+                    fono = textoFono,
+                    contacto = textoContacto,
+                    e_mail = textoEmail,
+                    direccion = textoDireccion,
+                    comuna = textoComuna
+                };
+                var response = proveedorDao.Save(obj);
+
+                MessageBox.Show("Proveedor Añadido Exitosamente", "Result", MessageBoxButton.OK, MessageBoxImage.Information);
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("proveedor no Añadido");
+            }
+        }
+    }
+}
